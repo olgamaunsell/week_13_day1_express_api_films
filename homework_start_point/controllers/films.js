990ff374b7430f2e77b776403f97423dcac6730f
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-02-26 16:41:36 +0000

    initial commit

---
+++ homework_start_point/controllers/films.js
@@ -0,0 +1,4 @@
+//since we don't have a database we'll use our front end models at the moment
+const films = require('../client/src/models/films')();
+const Film = require('../client/src/models/film');
+const Review = require('../client/src/models/review');
